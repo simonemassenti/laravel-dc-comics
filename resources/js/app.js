47fe70914c90a1282bf16e25cfc65f4b2f7d5bb0
Simone Massenti <139779954+simonemassenti@users.noreply.github.com>
2024-01-18 14:33:03 +0100

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+import '~resources/scss/app.scss';
+
+//Per processare le immagini
+import.meta.glob(["../img/**"]);
+
+//importiamo la parte JS di bootstrap
+import * as bootstrap from 'bootstrap';
